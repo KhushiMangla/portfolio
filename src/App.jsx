98eb91c09e744281d1aa86dada9fb4b935fda1ac
KhushiMangla <Khushimangla99@gmail.com>
2023-10-18 15:01:01 -0700

    added a inro content

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import Home from './components/Home'
+import Work from './components/Work'
+import Contact from './components/Contact'
+import About from './components/About'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const restBase = 'http://localhost:8888/portfolio/server/wp-json/wp/v2/'
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <header id="masthead" className="site-header">
+        <div className="site-branding">
+          <p className="site-title">Khushi's Portfolio App</p>
+        </div>
+        <nav className="site-navigation">
+          <ul>
+            <li><NavLink to='/' end>Home</NavLink></li>
+            <li><NavLink to='/about'>About</NavLink></li>
+            <li><NavLink to='/work'>Work</NavLink></li>
+            <li><NavLink to='/contact'>Contact</NavLink></li>
+          </ul>
+        </nav>
+      </header>
+      <main id="main">
+        <Routes>
+          <Route path='/' element={<Home restBase={restBase} />} />
+          <Route path='/about' element={<About restBase={restBase} />} />
+          <Route path='/work' element={<Work restBase={restBase} />} />
+          <Route path='/contact' element={<Contact restBase={restBase} />} />
+        </Routes>
+      </main>
+      <footer>
+        <p className="copyright">Created by <a href="https://Khushimangla.com/" target="_blank" rel="noopener noreferrer">Khushi</a>.</p>
+      </footer>
+    </Router>
   )
 }
 
