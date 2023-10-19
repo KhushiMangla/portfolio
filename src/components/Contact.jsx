75e47e72fc9670d6d159ba05bad547c3b54beb5a
KhushiMangla <Khushimangla99@gmail.com>
2023-10-19 12:11:38 -0700

    fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import Loading from './Loading'
 
-const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
+const Contact= ({ restBase }) => {
+    const restPath = restBase + 'pages/14'
+    const [restData, setData] = useState([])
+    const [isLoaded, setLoadStatus] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(restPath)
+            if (response.ok) {
+                const data = await response.json()
+                setData(data)
+                setLoadStatus(true)
+            } else {
+                setLoadStatus(false)
+            }
+        }
+        fetchData()
+    }, [restPath])
+
+    return (
+        <>
+            {isLoaded ?
+                <article id={`post-${restData.id}`}>
+                    <h1>{restData.title.rendered}</h1>
+                    <div className="entry-content">
+                        <section>
+                            <div dangerouslySetInnerHTML={{ __html: restData.content.rendered }} />
+                            <a href={`mailto:${restData.acf.email}`}>{restData.acf.email}</a>
+                            
+                        </section>
+                    </div>
+                </article>
+                :
+                <Loading />
+            }
+        </>
+    );
 }
 
 export default Contact
