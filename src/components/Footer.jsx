c1dff26ab6eb831b2a3e459ae31824ca5a75c0dc
KhushiMangla <Khushimangla99@gmail.com>
2023-10-24 10:55:16 -0700

    added styling to header

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+     <footer>
+        <p className="copyright">Created by <a href="https://Khushimangla.com/" target="_blank" rel="noopener noreferrer">Khushi Mangla</a>.</p>
+      </footer>
+    </>
+   
+  )
+}
+
+export default Footer
