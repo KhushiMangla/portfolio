0b55c597b70b46244a48b9c8e835b1201161a1ba
KhushiMangla <Khushimangla99@gmail.com>
2023-10-31 01:03:46 -0700

    added the loading spinner

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,25 @@
 import loading from '../images/loading.gif';
+import { useState, useEffect } from 'react';
+import HashLoader from 'react-spinners/ClipLoader'; // Import ClipLoader from 'react-spinners'
 
 const Loading = () => {
-    
-    return (
-        <img src={loading} alt="Loading" className="loading" id="loading" />
-    )
+    const [loading, setLoading] = useState(true); // Initialize loading as true
 
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false); // Set loading to false after 5000 milliseconds (5 seconds)
+        }, 5000);
+    }, []);
+
+    const override = `
+        display: block;
+        margin: 0 auto;
+    `;
+
+    return (
+        <HashLoader class="loading" color="red" loading={loading} css={override} size={100} /> // Use "loading" from state
+    );
 }
 
-export default Loading
+export default Loading;
+
