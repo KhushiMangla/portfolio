dc1d516f60297beb84581fd71f1d7920a87fb755
KhushiMangla <Khushimangla99@gmail.com>
2023-10-31 17:30:46 -0700

    fix the error of class grid

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,23 @@
-import loading from '../images/loading.gif';
+import { useState, useEffect } from 'react';
+import ClipLoader from 'react-spinners/ClipLoader'; // Import ClipLoader from 'react-spinners'
 
 const Loading = () => {
-    
-    return (
-        <img src={loading} alt="Loading" className="loading" id="loading" />
-    )
+    const [loading, setLoading] = useState(true); // Initialize loading as true
 
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false); // Set loading to false after 5000 milliseconds (5 seconds)
+        }, 5000);
+    }, []);
+
+    const override = `
+    display: block;
+    margin: 0 auto;
+    `;
+
+    return (
+        <ClipLoader class="loading" color="#ff9248" loading={loading} css={override} size={100} /> // Use "loading" from state
+    );
 }
 
-export default Loading
+export default Loading;
