2c53e42e0b40788477184d9a9827c194954690fa
KhushiMangla <Khushimangla99@gmail.com>
2023-11-02 13:45:42 -0700

    fixing the typing effect in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -45,17 +45,17 @@ const Home = ({ restBase }) => {
                 <article id={`post-${restData.id}`}>
                     <div className='intro'>
                         <motion.div className="intro-left" variants={introVariants} initial="initial" animate="animate">
-                            <motion.section className="name" variants={introVariants}>
+                            <motion.section  className="name" variants={introVariants}>
                                 <div>{restData.acf.intro}</div>
                             </motion.section>
                             <motion.section className="tagline" variants={introVariants}>
                                 <p>{restData.acf.tagline}</p>
                             </motion.section>
-                            <motion.div>
+                            <motion.section>
                             <motion.button className="mywork-btn" variants={introVariants}>
                                 <NavLink activeClassName="active" to="/work">View my work</NavLink>
                             </motion.button>
-                            </motion.div>
+                            </motion.section>
                         </motion.div>
                         <section className="profile">
                             <div dangerouslySetInnerHTML={{ __html: restData.content.rendered }} />
