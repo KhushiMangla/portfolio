407ee12184f6496f7eba1780bd5c79c007358d58
KhushiMangla <Khushimangla99@gmail.com>
2023-11-06 19:17:54 -0800

    added the icons on header bottom nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,35 @@ import { NavLink, Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header id="masthead" className="site-header">
-        <div className="site-branding">
+      <div className="site-branding">
+        <Link className="logo" to="/"> Khushi.</Link>
+      </div>
+      <nav className="site-navigation">
+        <ul>
+          {/* remove the home from header */}
+          {/* <li><NavLink activeClassName="active" to="/" > Home </NavLink></li> */}
+          <li><NavLink activeClassName="active" to="/about">
+            <span className="icon"><ion-icon name="person-outline"></ion-icon>
+            </span>
+            <span class="text">About </span>
+            <span class="circle"></span>
 
-          <Link className="logo" to="/"> Khushi.</Link>
-        </div>
-        <nav className="site-navigation">
-          <ul>
-            {/* remove the home from header */}
-            {/* <li><NavLink activeClassName="active" to="/" > Home </NavLink></li> */}
-            <li><NavLink activeClassName="active" to="/about"> About </NavLink></li>
-            <li><NavLink activeClassName="active" to="/work">Work</NavLink></li>
-            {/* <li><button className='contact-btn'><NavLink to="/contact">Contact me</NavLink></button></li> */}
-            <li><NavLink to="/contact">Contact me</NavLink></li>
-          </ul>
-        </nav>
-      </header>
+          </NavLink></li>
+          <li><NavLink activeClassName="active" to="/work">
+            <span className="icon"><ion-icon name="code-working-outline"></ion-icon></span>
+            <span className="text">Work</span>
+            <span className="circle"></span>
+          </NavLink></li>
+          <li><NavLink to="/contact">
+          <span className="icon"><ion-icon name="chatbubble-outline"></ion-icon></span>
+            <span className="text">Contact</span>
+            <span className="circle"></span>
+          </NavLink></li>
+          <div className="indicator"></div>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
-export default Header
+export default Header;
